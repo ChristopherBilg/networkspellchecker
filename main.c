19b3164bd9afe023544abd3c2bc4ce144ad5ec48
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2019-03-12 10:12:09 -0400

    Started work on the main.c file

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+#define DEFAULT_DICTIONARY "dictionary"
+#define DICTIONARY_SIZE 1000000
+#define DEFAULT_PORT 8010
+
+#define NUM_OF_PRODUCERS 5
+#define NUM_OF_CONSUMERS 8
+
+char dictionary[DICTIONARY_SIZE];
+
+int main(int argc, char **argv) {
+  return EXIT_SUCCESS;
+}
