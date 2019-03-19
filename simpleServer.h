612911c7d0ec3fe4d01f3db7de783a50dda8597d
Christopher R. Bilger <christopherbilg@gmail.com>
2019-03-19 08:17:09 -0400

    Cleaned up whitespace

--- simpleServer.h
+++ simpleServer.h
@@ -1,5 +1,6 @@
 #ifndef _SIMPLE_SERVER_H
 #define _SIMPLE_SERVER_H
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
@@ -7,5 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define BUF_LEN 512
+
 int open_listenfd(int);
+
 #endif
