c479397281a677214aa6b7ad169fd9a38932043f
Christopher R. Bilger <christopherbilg@gmail.com>
2019-03-31 08:44:24 -0400

    Test #2 completed

--- tests.c
+++ tests.c
@@ -1,12 +1,26 @@
+#include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "tests.h"
 
-void main() {
+#define NUM_THREADS 10
+
+int main(int argc, char **argv) {
   char *test = "Test Number: ";
 
   printf("%s1\n", test);
   printf("The required screenshots for this testing module are found in the /screenshots directory.\n\n");
 
   printf("%s2\n", test);
-  
+  pthread_t threads[NUM_THREADS];
+  for (int i=0; i<NUM_THREADS; i++)
+    threads[i] = pthread_create(&threads[i], NULL, &thread_print_prompt, NULL);
+
+  for (int i=0; i<NUM_THREADS; i++)
+    pthread_join(threads[i], NULL);
+}
+
+void *thread_print_prompt(void *params) {
+  printf("I am a thread running.\n");
+  return NULL;
 }
---
+++ tests.h
@@ -0,0 +1,7 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+int main(int argc, char **argv);
+void *thread_print_prompt(void *params);
+
+#endif
