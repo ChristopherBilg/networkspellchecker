dca6a03195573fa0ae25a585bfe3cd345597af06
Christopher R. Bilger <christopherbilg@gmail.com>
2019-03-31 09:07:57 -0400

    Testing req #4 completed

--- tests.c
+++ tests.c
@@ -1,12 +1,15 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "tests.h"
 #include "fifo_queue.h"
 #include "client.h"
 
 #define NUM_THREADS 3
 #define QUEUE_SIZE 10
+
+char dictionary_list[99171][256];
 
 int main(int argc, char **argv) {
   char *test = "Test Number: ";
@@ -35,6 +38,32 @@ int main(int argc, char **argv) {
   for (int i=0; i<QUEUE_SIZE+3; i++)
     dequeue(queue);
   printf("Dequeue of more than QUEUE_SIZE was successful.\n");
+
+  char input[100];
+  printf("Please input a test word for spell checking: ");
+  fgets(input, 100, stdin);
+
+  int input_len = strlen(input);
+  if (input[input_len-1] == '\n')
+    input[input_len-1] = '\0';
+
+  FILE *dictionary_fd = fopen("words.txt", "r");
+  for (int i=0; i<99171; i++)
+    fgets(dictionary_list[i], sizeof(dictionary_list[i]), dictionary_fd);
+  fclose(dictionary_fd);
+
+  for (int i=0; i<99171; i++) {
+    int str_len = strlen(dictionary_list[i]);
+    if (dictionary_list[i][str_len-1] == '\n')
+      dictionary_list[i][str_len-1] = '\0';
+  }
+
+  char *valid = " WRONG\n";
+  for (int i=0; i<99171; i++)
+    if (strcmp(input, dictionary_list[i]) == 0)
+      valid = " CORRECT\n";
+
+  printf("%s%s", input, valid);
 }
 
 void *thread_print_prompt(void *params) {
