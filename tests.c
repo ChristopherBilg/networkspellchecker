87d5376118e6b0142afb29ff2d27b8333b54da47
Christopher R. Bilger <christopherbilg@gmail.com>
2019-03-31 08:31:09 -0400

    The test files for the required project testing

---
+++ tests.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main() {
+  char *test = "Test Number: ";
+
+  printf("%s1\n", test);
+  printf("The required screenshots for this testing module are found in the /screenshots directory.\n\n");
+
+  printf("%s2\n", test);
+  
+}
